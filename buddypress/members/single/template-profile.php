095661637567f889b6b33a10df2d38d57bbf8d76
Daniel Le Blanc <daniel.leblanc@ctrlweb.ca>
2017-08-21 20:42:16 -0400

    bugfix in template profile: displaying or hiding badges properly

--- buddypress/members/single/template-profile.php
+++ buddypress/members/single/template-profile.php
@@ -1,11 +1,10 @@
 <?php
+// Get current displayed user ID
+$userID             = bp_displayed_user_id();
+$currentUserData    = get_userdata( $userID );
+$currentUserNiceUrl = get_permalink( get_option( 'bp-pages' )['members'] ) . $currentUserData->user_nicename;
 
-	// Get current displayed user ID
-	$userID = bp_displayed_user_id();
-	$currentUserData = get_userdata($userID);
-	$currentUserNiceUrl = get_permalink(get_option('bp-pages')['members']) . $currentUserData->user_nicename;
-
-	get_header();
+get_header();
 ?>
 
 
@@ -13,51 +12,54 @@
     <section class="profile-members-badges">
         <div class="profile-members-badges-heading">
             <span class="separator-prefix"></span>
-            <h3 class="profile-members-badges-heading-title"><?php _e('Badges Portfolio', 'badgefactor-theme'); ?></h3>
+            <h3 class="profile-members-badges-heading-title"><?php _e( 'Badges Portfolio', 'badgefactor-theme' ); ?></h3>
         </div>
         <ul class="profile-members-badges-list">
 
-            <?php
-                $htmlTemplates = '';
-                if (!$GLOBALS['badgefactor']->get_user_achievements($userID)) {
-                    echo "<li class=\"profile-members-no-badge\">". __("This member hasn't earned a badge for the moment.", 'badgefactor-theme') . "</li>";
-                } else {
-                    foreach ($GLOBALS['badgefactor']->get_user_achievements($userID) as $achievement) {
+			<?php
+			$htmlTemplates = '';
+			if ( ! $GLOBALS['badgefactor']->get_user_achievements( $userID ) ) {
+				echo "<li class=\"profile-members-no-badge\">" . __( "This member hasn't earned a badge for the moment.", 'badgefactor-theme' ) . "</li>";
+			} else {
+				foreach ( $GLOBALS['badgefactor']->get_user_achievements( $userID ) as $achievement ) {
 
-                        if (!$GLOBALS['badgefactor']->is_achievement_private($achievement->badge_id) || $userID == get_current_user_id())
-                        {
-                            $badgePost = get_post($achievement->badge_id);
+					if ( ! $GLOBALS['badgefactor']->is_achievement_private( $achievement->ID ) || $userID == get_current_user_id() ) {
+						$badgePost = get_post( $achievement->ID );
 
-                            $currentBadgeUrl = '/?badges='.$badgePost->post_name.'&member='.$currentUserData->user_nicename;
+						$r = preg_match( "/(.*?)(-\d+)$/", $badgePost->post_name, $matches );
+						if ( $r > 0 ) {
+							$badgePost->post_name = $matches[1];
+
+						}
+
+						$currentBadgeUrl = '/?badges=' . $badgePost->post_name . '&member=' . $currentUserData->user_nicename;
 
 
-                            $htmlTemplates .= '<li class="profile-members-badge"><figure class="profile-members-badge-figure">
-                            <a href="' . $currentBadgeUrl . '">' . badgeos_get_achievement_post_thumbnail( $achievement->achievement_id ) . '</a>
+						$htmlTemplates .= '<li class="profile-members-badge"><figure class="profile-members-badge-figure">
+                            <a href="' . $currentBadgeUrl . '">' . badgeos_get_achievement_post_thumbnail( $achievement->ID ) . '</a>
                             <figcaption class="profile-members-badge-details">
-                                <span class="profile-members-badge-description">'.get_the_title( $achievement->achievement_id ) .'</span>
+                                <span class="profile-members-badge-description">' . get_the_title( $achievement->ID ) . '</span>
                             </figcaption>';
-                            if ($userID == get_current_user_id())
-                            {
-                                switch ($GLOBALS['badgefactor']->is_achievement_private($achievement->badge_id))
-                                {
-                                    case true:
-                                        $htmlTemplates .= '<button class="private-status private" data-achievement-id="'. $achievement->badge_id . '"><span class="glyphicon glyphicon-eye-close" aria-hidden="true"></span></button>';
-                                        break;
-                                    case false:
-                                    default:
-                                        $htmlTemplates .= '<button class="private-status public" data-achievement-id="'. $achievement->badge_id . '"><span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span></button>';
-                                        break;
-                                }
-                            }
-                            $htmlTemplates .= '</figure></li>';
-                        }
-                    }
-                }
+						if ( $userID == get_current_user_id() ) {
+							switch ( $GLOBALS['badgefactor']->is_achievement_private( $achievement->ID ) ) {
+								case true:
+									$htmlTemplates .= '<button class="private-status private" data-achievement-id="' . $achievement->ID . '"><span class="glyphicon glyphicon-eye-close" aria-hidden="true"></span></button>';
+									break;
+								case false:
+								default:
+									$htmlTemplates .= '<button class="private-status public" data-achievement-id="' . $achievement->ID . '"><span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span></button>';
+									break;
+							}
+						}
+						$htmlTemplates .= '</figure></li>';
+					}
+				}
+			}
 
-                echo do_shortcode($htmlTemplates);
-            ?>
+			echo do_shortcode( $htmlTemplates );
+			?>
         </ul>
     </section>
 </div>
 
-<?php get_footer();?>
+<?php get_footer(); ?>
